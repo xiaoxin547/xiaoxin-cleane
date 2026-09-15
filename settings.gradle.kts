39cf084b122acc7075af3b8fbd212df0c6ed1066
xiaoxin547 <3921704252@qq.com>
2026-09-15 14:02:53 +0800

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "小鑫清理"
+include(":app")
